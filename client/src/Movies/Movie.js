67f40b7e70822d879b514369089a43eeee556b5f
Robby <robby.olson11@gmail.com>
2019-11-12 13:27:11 -0800

    DRYed

--- client/src/Movies/Movie.js
+++ client/src/Movies/Movie.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
+import MovieCard from "./MovieCard";
 
 const Movie = props => {
   const [movie, setMovie] = useState();
@@ -30,27 +31,11 @@ const Movie = props => {
     return <div>Loading movie information...</div>;
   }
 
-  const { title, director, metascore, stars } = movie;
+  const save = <div className="save-button">Save</div>;
   return (
-    <div className="save-wrapper">
-      <div className="movie-card">
-        <h2>{title}</h2>
-        <div className="movie-director">
-          Director: <em>{director}</em>
-        </div>
-        <div className="movie-metascore">
-          Metascore: <strong>{metascore}</strong>
-        </div>
-        <h3>Actors</h3>
-
-        {stars.map(star => (
-          <div key={star} className="movie-star">
-            {star}
-          </div>
-        ))}
-      </div>
-      <div className="save-button">Save</div>
-    </div>
+    <>
+      <MovieCard movie={movie} save={save}></MovieCard>
+    </>
   );
 };
 
